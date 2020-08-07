055b400fedf52e59e15e1aebf7a0b29d33aa1c38
Rafael Ferreira <rafaelblink@gmail.com>
2020-08-07 01:31:37 -0300

    ng scaffold with ng 10, material added and routes configured.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { MatSliderModule } from '@angular/material/slider';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { CurrentComponent } from './training/current/current.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentComponent,
+    NewTrainingComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatSliderModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
